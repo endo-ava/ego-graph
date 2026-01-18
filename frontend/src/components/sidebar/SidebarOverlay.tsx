091cd5fd3cff18ff3efca1f644717a5b0386e50b
ryuto.endo <ryuto9007@gmail.com>
2026-01-18 14:02:12 +0000

    Add new sidebar components: NewChatButton, SidebarHeader, SidebarOverlay, ThreadListEmpty, ThreadListError, ThreadListLoading

---
+++ frontend/src/components/sidebar/SidebarOverlay.tsx
@@ -0,0 +1,20 @@
+import { useChatStore } from '@/lib/store';
+
+export function SidebarOverlay() {
+  const { sidebarOpen, setSidebarOpen, onSidebarClose } = useChatStore();
+
+  if (!sidebarOpen) return null;
+
+  const handleClose = () => {
+    onSidebarClose?.();
+    setSidebarOpen(false);
+  };
+
+  return (
+    <div
+      className="fixed inset-0 bg-black/50 z-40 md:hidden"
+      onClick={handleClose}
+      aria-hidden="true"
+    />
+  );
+}
